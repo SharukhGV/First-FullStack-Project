04346584e5eef26825cd8f8d3c6b2acf85e668bf
SharukhGV <sharukhvelupillai@pursuit.org>
2023-01-24 07:33:35 -0500

    changed JSON data default data

--- server/models/transactions.js
+++ server/models/transactions.js
@@ -3,22 +3,22 @@ module.exports = [
       name: "Beginning Balance",
       date: "2023-01-12",
       amount: 0,
-      from: "fabs",
-      category: "heart-inc"
+      from: "Fabbulous Starting Balance",
+      category: "hope-inc"
     },
     {
-        name: "Second Expense",
-        date: "2023-01-12",
-        amount: 5,
-        from: "working hard",
-        category: "some category"
+        name: "First Income",
+        date: "2023-03-29",
+        amount: 120000,
+        from: "Software Engineer Job",
+        category: "Salary Yearly"
       },
       {
         name: "First Expense",
-        date: "2023-01-17",
-        amount: 3,
-        from: "99 cents store",
-        category: "some category"
+        date: "2024-30-10",
+        amount: -120000,
+        from: "Company Establishment",
+        category: "VeluPLAY/SGVcreations"
       },
   ];
   
